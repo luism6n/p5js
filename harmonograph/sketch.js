f8e49efa3053803c5ba244ebd423f6cc39b0d877
Luís Möllmann <luism6n@gmail.com>
2022-02-28 01:56:21 -0300

    add dynamic harmonograph sketch

--- harmonograph/sketch.js
+++ harmonograph/sketch.js
@@ -1,105 +1,70 @@
-let gui;
-let phaseInitial = 0,
-  phaseMax = 3.2,
-  phaseMin = 0,
-  phaseStep = 0.1;
-let freqInitial = 1,
-  freqMax = 10,
-  freqMin = 0,
-  freqStep = 0.1;
-
-let params = {
-  numPoints: 20,
-  numPointsMax: 300,
-  a1: 100,
-  a2: 100,
-  a3: 100,
-  a4: 100,
-  f1: freqInitial,
-  f2: freqInitial,
-  f3: freqInitial,
-  f4: freqInitial,
-  f1Max: freqMax,
-  f2Max: freqMax,
-  f3Max: freqMax,
-  f4Max: freqMax,
-  f1Min: freqMin,
-  f2Min: freqMin,
-  f3Min: freqMin,
-  f4Min: freqMin,
-  f1Step: freqStep,
-  f2Step: freqStep,
-  f3Step: freqStep,
-  f4Step: freqStep,
-  p1: phaseInitial,
-  p2: phaseInitial,
-  p3: phaseInitial,
-  p4: phaseInitial,
-  p1Max: phaseMax,
-  p2Max: phaseMax,
-  p3Max: phaseMax,
-  p4Max: phaseMax,
-  p1Min: phaseMin,
-  p2Min: phaseMin,
-  p3Min: phaseMin,
-  p4Min: phaseMin,
-  p1Step: phaseStep,
-  p2Step: phaseStep,
-  p3Step: phaseStep,
-  p4Step: phaseStep,
-  d1: 100,
-  d2: 100,
-  d3: 100,
-  d4: 100,
-};
+// Parameters
+let p = null;
+let offset = null;
 
 function setup() {
   createCanvas(windowWidth - 100, windowHeight - 100);
 
-  gui = createGui("Harmonograph");
-  gui.addObject(params);
-  // gui.addGlobals(
-  //   "numPoints",
-  //   "a1",
-  //   "a2",
-  //   "a3",
-  //   "a4",
-  //   "f1",
-  //   "f2",
-  //   "f3",
-  //   "f4",
-  //   "p1",
-  //   "p2",
-  //   "p3",
-  //   "p4",
-  //   "d1",
-  //   "d2",
-  //   "d3",
-  //   "d4"
-  // );
+  colorMode(RGB, 100);
+  frameRate(50);
+
+  p = {
+    numPoints: 750,
+    a1: (t) => 100,
+    a2: (t) => 100,
+    a3: (t) => 100,
+    a4: (t) => 100,
+    p1: (t) => (PI * t) / 12,
+    p2: (t) => PI,
+    p3: (t) => PI / 2,
+    p4: (t) => (PI * t) / 5,
+    f1: (t) => 14 + sin((PI * t) / 25),
+    f2: (t) => 14 + cos((PI * t) / 35),
+    f3: (t) => 15 + 2 * sin((PI * t) / 35),
+    f4: (t) => 14,
+    d1: (t) => 0.00001,
+    d2: (t) => 0.00001,
+    d3: (t) => 0.00001,
+    d4: (t) => 0.00001,
+  };
+
+  offset = 100 * random();
 }
 
 function draw() {
-  background(220);
-
-  drawHarmonograph();
+  background(25);
+  t = millis() / 1000 + offset;
+  drawHarmonograph(t);
 }
 
-function drawHarmonograph() {
+function drawHarmonograph(t) {
   push();
 
   translate(width / 2, height / 2);
 
-  for (let n = 0; n < params.numPoints * 2; n++) {
+  const c1 = color(100, 30, 20, 70 + 30 * sin((PI * t) / 8));
+  const c2 = color(20, 100, 100, 70 + 30 * sin((PI * t) / 8));
+  fill(lerpColor(c1, c2, 0.5 + 0.5 * cos((PI * t) / 4)));
+  noStroke();
+  for (let n = 0; n < p.numPoints; n++) {
     const x =
-      params.a1 * sin((params.f1 * PI * n) / params.numPoints + params.p1) +
-      params.a2 * sin((params.f2 * PI * n) / params.numPoints + params.p2);
+      p.a1(t) *
+        sin((p.f1(t) * PI * n) / p.numPoints + p.p1(t)) *
+        exp(-p.d1(t) * n) +
+      p.a2(t) *
+        sin((p.f2(t) * PI * n) / p.numPoints + p.p2(t)) *
+        exp(-p.d2(t) * n);
 
     const y =
-      params.a3 * sin((params.f3 * PI * n) / params.numPoints + params.p3) +
-      params.a4 * sin((params.f4 * PI * n) / params.numPoints + params.p4);
+      p.a3(t) *
+        sin((p.f3(t) * PI * n) / p.numPoints + p.p3(t)) *
+        exp(-p.d3(t) * n) +
+      p.a4(t) *
+        sin((p.f4(t) * PI * n) / p.numPoints + p.p4(t)) *
+        exp(-p.d4(t) * n);
 
-    ellipse(x, y, 3);
+    // console.log({ x, y });
+    ellipse(x, y, 4 * exp(0.0001 * n));
   }
 
   pop();
